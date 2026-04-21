33e2d0e01f5d5f71186daaa84530e924a90c5433
DorineCo <dorinecoquette@hotmail.com>
2026-04-21 18:38:07 +0200

    adding auth files

---
+++ backend/middleware/auth.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+
+const verifyToken = (req, res, next) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        const err = new Error("Accès refusé, vous devez être connecté.");
+        err.status = 401;
+        return next(err);
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.SESSION_SECRET);
+        req.user = decoded;
+        next();
+        
+    } catch {
+        const err = new Error("Token invalide ou expiré.");
+        err.status = 401;
+        return next(err);
+    }
+};
+
+export default verifyToken;
